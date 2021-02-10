70ba7d936486a7a932cf138985e34ea35940d098
naccachefred <32555472+naccachefred@users.noreply.github.com>
2021-02-10 12:01:13 +0100

    push

--- src/test/java/com/urbanisationsi/microservices_portefeuille/MicroservicePortefeuilleApplicationTests.java
+++ src/test/java/com/urbanisationsi/microservices_portefeuille/MicroservicePortefeuilleApplicationTests.java
@@ -3,11 +3,14 @@ package com.urbanisationsi.microservices_portefeuille;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class MicroservicePortefeuilleApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertTrue(false);
 	}
 
 }
