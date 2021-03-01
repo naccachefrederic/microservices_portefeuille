85d62743fe7c2ceec69fab0b1b1d48967b7b8f28
naccachefred <32555472+naccachefred@users.noreply.github.com>
2021-03-01 13:11:12 +0100

    push

--- src/test/java/com/urbanisationsi/microservices_portefeuille/MicroservicePortefeuilleApplicationTests.java
+++ src/test/java/com/urbanisationsi/microservices_portefeuille/MicroservicePortefeuilleApplicationTests.java
@@ -1,22 +1,13 @@
 package com.urbanisationsi.microservices_portefeuille;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-
-import java.util.List;
-
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import com.urbanisationsi.microservices_portefeuille.exceptions.PortefeuilleIntrouvableException;
-import com.urbanisationsi.microservices_portefeuille.http.controleur.PortefeuilleControleur;
-import com.urbanisationsi.microservices_portefeuille.modele.Portefeuille;
 
 @SpringBootTest
 class MicroservicePortefeuilleApplicationTests {
+
 	@Test
 	void contextLoads() {
-		assertThat(true);
 	}
+
 }
